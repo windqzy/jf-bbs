bfaccf63446e082abaf68bd98125e7bbfc8d9bcb
zqian <zqian1994@126.com>
2019-02-28 16:55:24 +0800

    用户中心

--- ui/src/permission.js
+++ ui/src/permission.js
@@ -3,7 +3,7 @@ import router from './router';
 import store from './store';
 
 const whiteList = ['/user/reg', '/'];// 重定向白名单
-
+console.log(window.localStorage['B-Token'])
 router.beforeEach((to, from, next) => {
   if (window.localStorage['B-Token']) {
     if (!store.getters.user.username) {
@@ -18,13 +18,11 @@ router.beforeEach((to, from, next) => {
       next()
     }
   } else {
-    next('/');
     //  不存在用户姓名
-    console.log(to.path);
-    // if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
-    //   next()
-    // }
-
+    if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
+      next()
+    }
+    next('/');
     // store.dispatch('addUserInfo').then(() => {
     //   console.log('刷新', store.getters.user.username)
     //   if (!store.getters.user.username) {

f309f859477dc50bca95e445ad2e8c46859c3f40
mjp <mjp7177@163.com>
2019-06-20 10:39:31 +0800

    Faq的主表实体类相关

---
+++ src/main/java/com/jfsoft/bbs/dao/BbsFaqDao.java
@@ -0,0 +1,9 @@
+package com.jfsoft.bbs.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jfsoft.bbs.entity.BbsFaqEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BbsFaqDao extends BaseMapper<BbsFaqEntity> {
+}
---
+++ src/main/java/com/jfsoft/bbs/entity/BbsFaqEntity.java
@@ -0,0 +1,66 @@
+package com.jfsoft.bbs.entity;
+
+import java.util.Date;
+
+public class BbsFaqEntity {
+
+    private Integer id;
+
+    private String question;
+
+    private String answer;
+
+    private Integer userId;
+
+    private Date initTime;
+
+    private Integer typeId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getInitTime() {
+        return initTime;
+    }
+
+    public void setInitTime(Date initTime) {
+        this.initTime = initTime;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+}

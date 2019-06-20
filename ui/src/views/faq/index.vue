0f298b644f156338b9e95d43aed9e84f73066030
zqian <zqian1994@126.com>
2019-06-20 15:00:07 +0800

    FAQ

--- ui/src/views/faq/index.vue
+++ ui/src/views/faq/index.vue
@@ -1,123 +1,121 @@
 <template>
-    <div class="layui-container">
-      <!--<el-row :gutter="10">-->
-        <!--<el-col :lg="18" :xs="24">-->
-          <!--<el-card shadow="never">-->
-            <!--<div slot="header">-->
-              <!--<el-input placeholder="请输入产品或问题" autofocus>-->
-                <!--<el-button slot="append" type="primary" icon="el-icon-search"></el-button>-->
-              <!--</el-input>-->
-            <!--</div>-->
-            <!--<div class="faq_cont_list">-->
-              <!--<dl>-->
-                <!--<dd v-for="i in 5">-->
-                  <!--<a href="">选中一个单元格输入，输入括号（）以后，光标会飘到前面一个单元格，怎么办？</a>-->
-                <!--</dd>-->
-              <!--</dl>-->
-            <!--</div>-->
-          <!--</el-card>-->
-        <!--</el-col>-->
-        <!--<el-col :lg="6" :xs="24">-->
-          <!--<el-card shadow="never">-->
-            <!--<div slot="header">-->
-              <!--<span style="font-size: 18px">热门问题</span>-->
-            <!--</div>-->
-          <!--</el-card>-->
-        <!--</el-col>-->
-      <!--</el-row>-->
-      <el-row :gutter="10">
-        <el-col :lg="6" :xs="24">
-          <el-tabs type="border-card" class="md8">
-            <el-tab-pane label="FAQ分类">
-              <el-tree
-                default-expand-all
-                :data="labelList"
-                :props="defaultProps"
-                @node-click="handleNodeClick">
-              </el-tree>
-            </el-tab-pane>
-          </el-tabs>
-        </el-col>
-        <el-col :lg="18" :xs="24" v-show="listBox">
-          <el-input placeholder="请输入关键字"></el-input>
-          <el-tabs type="border-card">
-            <el-tab-pane label="FAQ列表">
-
+  <div class="layui-container">
+    <!--<el-row :gutter="10">-->
+    <!--<el-col :lg="18" :xs="24">-->
+    <!--<el-card shadow="never">-->
+    <!--<div slot="header">-->
+    <!--<el-input placeholder="请输入产品或问题" autofocus>-->
+    <!--<el-button slot="append" type="primary" icon="el-icon-search"></el-button>-->
+    <!--</el-input>-->
+    <!--</div>-->
+    <!--<div class="faq_cont_list">-->
+    <!--<dl>-->
+    <!--<dd v-for="i in 5">-->
+    <!--<a href="">选中一个单元格输入，输入括号（）以后，光标会飘到前面一个单元格，怎么办？</a>-->
+    <!--</dd>-->
+    <!--</dl>-->
+    <!--</div>-->
+    <!--</el-card>-->
+    <!--</el-col>-->
+    <!--<el-col :lg="6" :xs="24">-->
+    <!--<el-card shadow="never">-->
+    <!--<div slot="header">-->
+    <!--<span style="font-size: 18px">热门问题</span>-->
+    <!--</div>-->
+    <!--</el-card>-->
+    <!--</el-col>-->
+    <!--</el-row>-->
+    <el-row :gutter="10">
+      <el-col :lg="6" :xs="24" class="faq-tree">
+        <el-button size="mini" class="extra" type="primary">新增</el-button>
+        <el-tabs type="border-card" class="md8">
+          <el-tab-pane label="FAQ分类">
+            <el-tree
+              default-expand-all
+              :data="labelList"
+              :props="defaultProps"
+              @node-click="handleNodeClick">
+            </el-tree>
           </el-tab-pane>
-          <el-tab-pane label="热门问题">
-
-            </el-tab-pane>
-            <ul>
-              <li v-for="(item, index) in faqList">
-                <el-link type="primary" @click="faqDetail(item)">{{index + 1}}{{item.question}}</el-link>
-              </li>
-            </ul>
-          </el-tabs>
-        </el-col>
-        <el-col :lg="18" :xs="24" v-show="detailBox">
-          <el-card shadow="never">
-            <div slot="header">
-              <el-page-header @back="goBack" :content="toFaq.question">
-              </el-page-header>
-            </div>
-            <p v-html="toFaq.answer"></p>
-          </el-card>
-        </el-col>
-      </el-row>
-    </div>
+        </el-tabs>
+      </el-col>
+      <el-col :lg="18" :xs="24" v-show="listBox" class="faq-tabs">
+        <el-input placeholder="请输入关键字" size="mini"></el-input>
+        <el-tabs type="border-card">
+          <el-tab-pane label="FAQ列表"></el-tab-pane>
+          <el-tab-pane label="热门问题"></el-tab-pane>
+          <ul>
+            <li v-for="(item, index) in faqList">
+              <el-link type="primary" @click="faqDetail(item)">
+                <span>{{index + 1}}</span>. {{item.question}}
+              </el-link>
+            </li>
+          </ul>
+        </el-tabs>
+      </el-col>
+      <el-col :lg="18" :xs="24" v-show="detailBox">
+        <el-card shadow="never">
+          <div slot="header">
+            <el-page-header @back="goBack" :content="toFaq.question"></el-page-header>
+          </div>
+          <p v-html="toFaq.answer"></p>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import * as faq from '@/api/faq';
+  import * as faq from '@/api/faq';
 
-    export default {
-      name: "index",
-      data() {
-        return {
-          listBox: true,
-          detailBox: false,
-          labelList:[],
-          faq:'',
-          faqList: [],
-          toFaq:'',
-          defaultProps: {
-            id: 'id',
-            children: 'children',
-            label: 'name',
-            parentId: 'parentId'
-          },
-        }
+  export default {
+    name: "index",
+    data() {
+      return {
+        listBox: true,
+        detailBox: false,
+        labelList: [],
+        faq: '',
+        faqList: [],
+        toFaq: '',
+        defaultProps: {
+          id: 'id',
+          children: 'children',
+          label: 'name',
+          parentId: 'parentId'
+        },
+      }
+    },
+    created() {
+      this.getFaq();
+    },
+    methods: {
+      getFaq() {
+        faq.getFaq().then(res => {
+          this.labelList = res.data;
+        })
       },
-      created() {
-        this.getFaq();
+      getFaqList(typeId) {
+        faq.getFaqList(typeId).then(res => {
+          this.faqList = res.data;
+        })
       },
-      methods: {
-        getFaq(){
-          faq.getFaq().then(res => {
-            this.labelList = res.data;
-          })
-        },
-        getFaqList(typeId){
-          faq.getFaqList(typeId).then(res => {
-            this.faqList = res.data;
-          })
-        },
-        handleNodeClick(data) {
-          this.faq = data;
-          console.log(this.faq);
-          this.getFaqList(this.faq.id);
-        },
-        faqDetail(item) {
-          this.listBox = false;
-          this.detailBox = true;
-          this.toFaq = item;
-        },
-        goBack() {
-          this.listBox = true;
-          this.detailBox = false;
-        }
+      handleNodeClick(data) {
+        this.faq = data;
+        console.log(this.faq);
+        this.getFaqList(this.faq.id);
+      },
+      faqDetail(item) {
+        this.listBox = false;
+        this.detailBox = true;
+        this.toFaq = item;
+      },
+      goBack() {
+        this.listBox = true;
+        this.detailBox = false;
       }
     }
+  }
 </script>
 
 <style scoped lang="scss">
--- ui/src/views/post/detail.vue
+++ ui/src/views/post/detail.vue
@@ -429,9 +429,10 @@
       },
       /* 预览 */
       preview(item) {
+        console.log(item)
         let url = encodeURIComponent(item.url); //要预览文件的访问地址
         let winHeight = window.document.documentElement.clientHeight - 10;
-        window.open('http://127.0.0.1:8849/onlinePreview?url=' + url, '_blank', 'height=' + winHeight + ',top=80,left=80,toolbar=no,menubar=no,scrollbars=yes,resizable=yes');
+        window.open('http://172.17.0.203:8849/onlinePreview?url=' + url, '_blank', 'height=' + winHeight + ',top=80,left=80,toolbar=no,menubar=no,scrollbars=yes,resizable=yes');
       },
       /* 下载附件 */
       download(item) {
